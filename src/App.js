1434b66b9a69a27f38b99e4273be6fc4302f8d12
FOUIX <fouix@pc-155.home>
2020-12-01 13:51:47 +0100

    search

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ import Home from "./containers/Home";
 //import des composants
 import Header from "./components/Header";
 
+//import icons
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+library.add(faSearch);
+
 function App() {
   return (
     <Router>
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,42 +2,54 @@ import React from "react";
 
 import Logo from "../assets/accueil.webp";
 
+//import icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 //import du json
 import data from "../assets/restaurants.json";
 //console.log(data); //vérification de mon fichier json
 const result = data.filter((item) => item.category === 0);
-console.log(result);
+//console.log(result);
+
 const Home = () => {
   return (
     <div className="resto-container">
-      <div className="form-container">
-        <div className="form">
-          <h2>Find Vegan Restaurants Nearby</h2>
-          <form>
-            <input type="text"></input>
-            <input type="submit"></input>
-          </form>
-        </div>
+      <div className="form">
+        <h2>Find Vegan Restaurants Nearby</h2>
+        <form>
+          <input type="text"></input>
+          <button type="submit">
+            <FontAwesomeIcon icon="search" className="icons" />
+          </button>
+        </form>
+      </div>
 
-        <img src={Logo} alt="accueil" className="image-accueil" />
+      <img src={Logo} alt="accueil" className="image-accueil" />
+
+      <h2>Vegan Restaurant Near Me</h2>
+      <div className="resto">
+        {result.map((item, index) => {
+          if (index < 10) {
+            return (
+              <div className="resto-infos">
+                {item.pictures[0] && (
+                  <img
+                    src={item.pictures[0]}
+                    alt={item.name}
+                    className="photo-resto"
+                  ></img>
+                )}
+
+                <h1>{item.name}</h1>
+                <p>{item.description}</p>
+              </div>
+            );
+          } else {
+            return null;
+          }
+        })}
       </div>
       {/* <p>{data[0].name}</p> */}
-      {result.map((item, index) => {
-        return (
-          <div className="resto-infos">
-            {item.pictures[0] && (
-              <img
-                src={item.pictures[0]}
-                alt={item.name}
-                className="photo-resto"
-              ></img>
-            )}
-
-            <h1>{item.name}</h1>
-            <p>{item.description}</p>
-          </div>
-        );
-      })}
     </div>
   );
 };
