98f7fc90237db53bd0962a2b94ea65e533440f87
FOUIX <fouix@pc-155.home>
2020-12-08 17:45:48 +0100

    id

--- src/containers/Detail.js
+++ src/containers/Detail.js
@@ -1,12 +1,30 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
 //import du json
 import data from "../assets/restaurants.json";
 
 const Detail = () => {
+  const params = useParams();
+  let id = params.id;
+  console.log(useParams());
+  //console.log(typeof id);
+  const getDetail = () => {
+    for (let i = 0; i < data.length; i++) {
+      console.log(typeof data[i].placeId);
+      // if (data[i].placeId === Number(id)) {
+      if (String(data[i].placeId === id)) {
+        return data[i];
+      }
+    }
+  };
+
+  const detail = getDetail();
+  console.log("detail=>", detail);
   return (
     <div>
       <h1>Details</h1>
-      <p>{data.name}</p>
+      {/* <p>{detail.name}</p> */}
     </div>
   );
 };
